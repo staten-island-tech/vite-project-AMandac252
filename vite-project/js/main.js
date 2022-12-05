f4afa30a2d38f734a3f5874e97b8e42b6c3bc99d
AMandac252 <113184877+AMandac252@users.noreply.github.com>
2022-12-05 10:10:01 -0500

    added the images
    DO this at home and set up the border and the basic ass front page
    
    - The two cards in the beginning side by side should  be their infographics
    -  the first box that has all the general information should link to their joint channel and then have that stay in the html
    = when the button is clicked the two lower cards should be replaced with each of their songs and a picture of each of them in each song seperatly
    
    - create a main button so that you can go back and access the two infographics
    - there should be 3 buttons total
    
    - kanae should be gray white, royal blue
    -kuzha should be red white and black
    -- accent color should be a gold and then the text should be white if the background is going to be a dark color
    
    - website main backgrond should be a dark navy blue and white text - gold accent or smth idk
    make it clean
    - do this at home
    
    tldr: 3 working buttons and pictures for proof that it works and then get all the other images that you need.

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -3,6 +3,7 @@ import { angel } from "../js/2434/angel";
 
 document.querySelector("#app").innerHTML = `
  <h1>Hellowwwwwww</h1> 
+ <div class="main-image"><img src="../vite-project/images/A.jpg" Calt=""></div>
 `;
 
 console.log(angel);
