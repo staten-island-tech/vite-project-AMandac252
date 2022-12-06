b3257ee6ef08b200a28f1887d10b546a732a8f67
AMandac252 <113184877+AMandac252@users.noreply.github.com>
2022-12-06 10:16:32 -0500

    fafdsafdsafdafds

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,9 +1,2 @@
 import "../styles/style.css";
 import { angel } from "../js/2434/angel";
-
-document.querySelector("#app").innerHTML = `
- <h1>Hellowwwwwww</h1> 
- <div class="main-image"><img src="../vite-project/images/A.jpg" Calt=""></div>
-`;
-
-console.log(angel);
