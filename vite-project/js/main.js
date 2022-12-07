ccee2749b3a7082404889d7a9642773ca16f3586
AMandac252 <113184877+AMandac252@users.noreply.github.com>
2022-12-06 21:33:44 -0500

    updated main info

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,2 +1,47 @@
 import "../styles/style.css";
 import { angel } from "../js/2434/angel";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
+
+const DOMSelectors = {
+  button: document.querySelector(".btn"),
+  display: document.querySelector("#display"),
+};
+
+/* DOMSelectors.button.addEventListener("click", function (e) {
+  e.preventDefault();
+  const title = DOMSelectors.title.value;
+  const img = DOMSelectors.image.value;
+  const artist = DOMSelectors.artist.value;
+
+  function addcard() {
+    DOMSelectors.display.insertAdjacentHTML(
+      "beforeend",
+
+      `<div class="display-card">
+                  <p class="display-album">${title}</p>
+                 <p><img class="display-img" src="${img}"></p>
+                  <p class ="display-artist">${artist}</p>
+                  <button class="remove btn">Remove Album</button>
+                </div>`
+    );
+
+    clearinput();
+  }
+  addcard();
+  document.querySelectorAll(".remove").forEach((element) =>
+    element.addEventListener("click", function (e) {
+      e.target.parentElement.remove();
+    })
+  );
+});
+console.log(DOMSelectors);
+
+function clearinput() {
+  DOMSelectors.title.value = "";
+  DOMSelectors.artist.value = "";
+  DOMSelectors.image.value = "";
+}
+ */
