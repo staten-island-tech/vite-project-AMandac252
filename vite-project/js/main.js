79002311adef41fdf59fbf48aeb171a20101d992
AMandac252 <113184877+AMandac252@users.noreply.github.com>
2022-12-07 10:16:39 -0500

    mmmmm

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -10,38 +10,12 @@ const DOMSelectors = {
   display: document.querySelector("#display"),
 };
 
-/* DOMSelectors.button.addEventListener("click", function (e) {
-  e.preventDefault();
-  const title = DOMSelectors.title.value;
-  const img = DOMSelectors.image.value;
-  const artist = DOMSelectors.artist.value;
+DOMSelectors.display.insertAdjacentHTML(
+  "beforeend",
 
-  function addcard() {
-    DOMSelectors.display.insertAdjacentHTML(
-      "beforeend",
-
-      `<div class="display-card">
-                  <p class="display-album">${title}</p>
-                 <p><img class="display-img" src="${img}"></p>
-                  <p class ="display-artist">${artist}</p>
-                  <button class="remove btn">Remove Album</button>
-                </div>`
-    );
-
-    clearinput();
-  }
-  addcard();
-  document.querySelectorAll(".remove").forEach((element) =>
-    element.addEventListener("click", function (e) {
-      e.target.parentElement.remove();
-    })
-  );
-});
-console.log(DOMSelectors);
-
-function clearinput() {
-  DOMSelectors.title.value = "";
-  DOMSelectors.artist.value = "";
-  DOMSelectors.image.value = "";
-}
- */
+  `<div data-aos="fade-down" class="display-card">
+  <img class="display-img" src="images/icebreaker.png"/>
+  <h5 class="display-album">IceBreaker</h5>
+   <button class="link-button">Listen now</button>
+  </div>`
+);
