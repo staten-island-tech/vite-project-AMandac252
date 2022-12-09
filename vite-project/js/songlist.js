1b6ad1b6eb4b23e1647ed1c0f2d4b53406df9cb8
AMandac252 <113184877+AMandac252@users.noreply.github.com>
2022-12-09 10:14:50 -0500

    jkhflkjfgdjlkgfdjlkfjlfjlk

--- vite-project/js/songlist.js
+++ vite-project/js/songlist.js
@@ -1,6 +1,19 @@
-let data [
-    {
-        song: Bloody Groovy, 
-    }
+const data = [
+  {
+    song: "【『Not For You』叶／葛葉】",
+    type: "single",
+    release: "Feb 11, 2020",
+  },
 
-]; 
+  {
+    song: "ChroNoiR【オリジナル曲】",
+    type: "single",
+    release: "Feb 11, 2020",
+  },
+
+  {
+    song: "【オリジナルMV】Ice breaker / ChroNoiR",
+    type: "cover",
+    release: "May 24, 2020",
+  },
+];
